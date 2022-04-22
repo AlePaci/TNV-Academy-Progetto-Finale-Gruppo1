7e23fd27a01e1c11217dab16f30df04288fd747d
AlePaci <paci.alessandro88@gmail.com>
2022-04-22 16:58:37 +0200

    conflitto 1

--- frontend/angular/GuessWhosBackTheMovie/src/app/app-routing.module.ts
+++ frontend/angular/GuessWhosBackTheMovie/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 
 const routes: Routes = [
   { path: 'registration', component: RegistrationComponent},
+  {path: 'login', component: LoginComponent},
   { path: '', redirectTo: '/', pathMatch: 'full'}
   
 ];
