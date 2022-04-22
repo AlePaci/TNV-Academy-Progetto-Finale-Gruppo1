b5055f804dd8dedb37704d629a06c6c65865ba00
MARCO_LODDO <marco.loddo86@gmail.com>
2022-04-22 11:00:49 +0200

    route app login

--- frontend/angular/GuessWhosBackTheMovie/src/app/app-routing.module.ts
+++ frontend/angular/GuessWhosBackTheMovie/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: 'registration', component: RegistrationComponent},
-  
+  {path: 'login', component: LoginComponent},
+
 ];
 
 @NgModule({
