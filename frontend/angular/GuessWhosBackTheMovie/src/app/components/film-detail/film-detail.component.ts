0c73ffc67a8476851a3bc6d7d327a28b6511bd96
MARCO_LODDO <marco.loddo86@gmail.com>
2022-04-23 16:49:08 +0200

    #templateprovvisorio

--- frontend/angular/GuessWhosBackTheMovie/src/app/components/film-detail/film-detail.component.ts
+++ frontend/angular/GuessWhosBackTheMovie/src/app/components/film-detail/film-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MovieDetails } from '../../model/movieDetails.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TMDBApiService } from '../../services/tmdb-api.service';
 
 @Component({
   selector: 'app-film-detail',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmDetailComponent implements OnInit {
 
-  constructor() { }
+  movieId: number;
+  movieDetail: MovieDetails | null = null
+
+  constructor(activatedRoute: ActivatedRoute,
+    private router : Router,
+    private httpClient: HttpClient) {
+    this.movieId = +activatedRoute.snapshot.params['movieId']
+   }
 
   ngOnInit(): void {
+  this.httpClient.get<MovieDetails>(`https://api.themoviedb.org/3/movie/${this.movieId}`).subscribe({
+    next: (res) => this.movieDetail = res
+  })
   }
 
+  navigate(amount:number) {
+    const movieId = this.movieId + amount
+    this.movieId = movieId;
+    this.router.navigateByUrl(`${movieId}`)
+    this.getData();
+
+  }
+  private getData() {
+
+  }
 }
+
+
