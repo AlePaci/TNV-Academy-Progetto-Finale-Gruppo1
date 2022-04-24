5c25bc6d6783925ac548af2a96ab56e0ac42254f
MARCO_LODDO <marco.loddo86@gmail.com>
2022-04-24 19:16:33 +0200

    #egoloMaNonAncora

--- frontend/angular/GuessWhosBackTheMovie/src/app/components/film-detail/film-detail.component.ts
+++ frontend/angular/GuessWhosBackTheMovie/src/app/components/film-detail/film-detail.component.ts
@@ -1,3 +1,5 @@
+import { Genre } from './../../model/movieDetails.model';
+
 
 
 import { TMDBApiService } from './../../services/tmdb-api.service';
@@ -6,6 +8,8 @@ import { HttpClient } from '@angular/common/http';
 import { MovieDetails } from '../../model/movieDetails.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MovieCredits } from '../../model/movieCredits.model';
+import { NgIfContext } from '@angular/common';
+
 
 
 @Component({
@@ -18,29 +22,61 @@ export class FilmDetailComponent implements OnInit {
 
   detail: MovieDetails |null = null
   credits: MovieCredits |null = null
+  poster: string = ''
+  genre: Genre[] = [];
+  movie1:number = 343611;
+
 
 
   constructor(activatedRoute: ActivatedRoute,
     private router : Router,
-    private TMDBApiService:TMDBApiService) {
+    public TMDBApiService:TMDBApiService) {
     activatedRoute.params.subscribe(val => this.credits)
     activatedRoute.params.subscribe(val => this.detail)
    }
+   private getData() {}
 
   ngOnInit(): void {
-    this.getData();
+
+    this.getData(); {
+      this.TMDBApiService.getMovieCredits( this.movie1).subscribe({
+        next: (res) => this.credits = res
+      }),
+      this.TMDBApiService.getMovieDetails(this.movie1).subscribe({
+        next: (res) => this.detail = res
+      })
+     this.TMDBApiService.getMoviePoster(`https://image.tmdb.org/t/p/original/${this.movie1}`)
+
+
+
+
+
 
   }
 
 
-  private getData() {
-    this.TMDBApiService.getMovieCredits(343611).subscribe({
+  /*private getData() {
+    this.TMDBApiService.getMovieCredits( this.movie1).subscribe({
       next: (res) => this.credits = res
     }),
-    this.TMDBApiService.getMovieDetails(343611).subscribe({
+    this.TMDBApiService.getMovieDetails(this.movie1).subscribe({
       next: (res) => this.detail = res
     })
+   this.TMDBApiService.getMoviePoster(`https://image.tmdb.org/t/p/original/${this.movie1}`)
+     return this.poster*/
+
+
+
+
+
+
+
   }
+
+
+
+
+
 }
 
 
