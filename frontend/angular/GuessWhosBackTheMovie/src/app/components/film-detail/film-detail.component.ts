c8503cddd116e7e33844c393b421708e83d27c81
MARCO_LODDO <marco.loddo86@gmail.com>
2022-04-28 19:55:58 +0200

    #aridaje

--- frontend/angular/GuessWhosBackTheMovie/src/app/components/film-detail/film-detail.component.ts
+++ frontend/angular/GuessWhosBackTheMovie/src/app/components/film-detail/film-detail.component.ts
@@ -1,9 +1,11 @@
+import { CommentsService } from './../../services/comments.service';
 import { TMDBApiService } from './../../services/tmdb-api.service';
 import { Component, OnInit } from '@angular/core';
-
 import { MovieDetails, Genre } from '../../model/movieDetails.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MovieCredits } from '../../model/movieCredits.model';
+import { RatingsService } from '../../services/ratings.service';
+import { Comment } from '../../model/comment.model';
 
 
 @Component({
@@ -18,12 +20,17 @@ export class FilmDetailComponent implements OnInit {
   credits: MovieCredits |null = null
   poster: string = ''
   genres: Genre[] | undefined = [];
+  comment: Comment |null = null;
+  rating: RatingsService| null = null;
 
-  movie1:number = 343611;
+  movie1 : number = 343611;
+  user : number = 1;
 
 
 
   constructor(private activatedRoute: ActivatedRoute,
+              private commenService: CommentsService,
+              private ratingService: RatingsService,
 
     public TMDBApiService:TMDBApiService) {
 
@@ -31,9 +38,13 @@ export class FilmDetailComponent implements OnInit {
    private getData() {}
 
   ngOnInit(): void {
+
+
+    this.commenService.getComment(this.user, this.movie1).subscribe({
+      next: (res)=>  this.comment = res
+    }),
+
       this.activatedRoute.params.subscribe((val) => this.movie1 = +val['movieId'])
-
-
       this.TMDBApiService.getMovieCredits( this.movie1).subscribe({
         next: (res: MovieCredits | null) => this.credits = res
       }),
@@ -62,4 +73,8 @@ export class FilmDetailComponent implements OnInit {
 
 
 
+
+function cc(cc: any) {
+  throw new Error('Function not implemented.');
+}
 
