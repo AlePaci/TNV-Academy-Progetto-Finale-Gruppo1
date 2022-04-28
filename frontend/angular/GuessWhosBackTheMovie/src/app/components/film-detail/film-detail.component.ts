bb46702ab046ba80785ae48c9fdc64cea5fecd26
MARCO_LODDO <marco.loddo86@gmail.com>
2022-04-28 12:27:14 +0200

    Merge branch 'main' of https://github.com/AlePaci/TNV-Academy-Progetto-Finale-Gruppo1

--- frontend/angular/GuessWhosBackTheMovie/src/app/components/film-detail/film-detail.component.ts
+++ frontend/angular/GuessWhosBackTheMovie/src/app/components/film-detail/film-detail.component.ts
@@ -1,10 +1,9 @@
 import { TMDBApiService } from './../../services/tmdb-api.service';
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
 import { MovieDetails, Genre } from '../../model/movieDetails.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MovieCredits } from '../../model/movieCredits.model';
-import { NgIfContext } from '@angular/common';
 
 
 @Component({
@@ -18,30 +17,30 @@ export class FilmDetailComponent implements OnInit {
   detail: MovieDetails |null = null
   credits: MovieCredits |null = null
   poster: string = ''
-  genres: Genre[] = [];
+  genres: Genre[] | undefined = [];
 
   movie1:number = 343611;
 
 
 
-  constructor(activatedRoute: ActivatedRoute,
-    private router : Router,
+  constructor(private activatedRoute: ActivatedRoute,
+
     public TMDBApiService:TMDBApiService) {
-    activatedRoute.params.subscribe(val => this.credits)
-    activatedRoute.params.subscribe(val => this.detail)
+
    }
    private getData() {}
 
   ngOnInit(): void {
+      this.activatedRoute.params.subscribe((val) => this.movie1 = +val['movieId'])
 
-     {
+
       this.TMDBApiService.getMovieCredits( this.movie1).subscribe({
-        next: (res) => this.credits = res
+        next: (res: MovieCredits | null) => this.credits = res
       }),
       this.TMDBApiService.getMovieDetails(this.movie1).subscribe({
-        next: (res) =>{
+        next: (res: MovieDetails | null) =>{
            this.detail = res;
-           this.genres = res.genres;
+           this.genres = res?.genres;
               }
            })
      this.TMDBApiService.getMoviePoster(`https://image.tmdb.org/t/p/original/${this.movie1}`)
@@ -50,7 +49,7 @@ export class FilmDetailComponent implements OnInit {
 
 
 }
-}
+
 
 
 
