88e406dea02089998dcb117de15566b9d6a4c76a
AlePaci <paci.alessandro88@gmail.com>
2022-04-30 07:31:48 +0200

    film view

--- frontend/angular/GuessWhosBackTheMovie/src/app/components/film-detail/film-detail.component.ts
+++ frontend/angular/GuessWhosBackTheMovie/src/app/components/film-detail/film-detail.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 
 import { MovieDetails, Genre } from '../../model/movieDetails.model';
 import { ActivatedRoute, Router } from '@angular/router';
-import { MovieCredits } from '../../model/movieCredits.model';
+import { Cast, Crew, MovieCredits } from '../../model/movieCredits.model';
+import { CommentsService } from 'src/app/services/comments.service';
+import { RatingsService } from 'src/app/services/ratings.service';
+import { SessionStorageService } from 'src/app/services/session-storage.service';
 
 
 @Component({
@@ -16,16 +19,21 @@ export class FilmDetailComponent implements OnInit {
 
   detail: MovieDetails |null = null
   credits: MovieCredits |null = null
+  director: Crew[] | null = null
   poster: string = ''
   genres: Genre[] | undefined = [];
+  comment: string |null= null;
+  rating: number[]| null = null;
+  movieid:number = 0;
 
-  movieid:number = 343611;
 
 
-
-  constructor(private activatedRoute: ActivatedRoute,
-
-    public movieService:TMDBApiService) {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    public movieService:TMDBApiService,
+    private commentService:CommentsService,
+    private ratingService: RatingsService,
+    private sessionServise:SessionStorageService) {
 
    }
    private getData() {}
@@ -35,18 +43,35 @@ export class FilmDetailComponent implements OnInit {
 
 
       this.movieService.getMovieCredits( this.movieid).subscribe({
-        next: (res: MovieCredits | null) => this.credits = res
-      }),
+        next: (res) =>{
+          this.credits = res;
+          this.director = this.credits.crew?.filter(crew => crew.job ==="Director");
+        },
+        error:(res) => console.log(res)
+      });
+
       this.movieService.getMovieDetails(this.movieid).subscribe({
-        next: (res: MovieDetails | null) =>{
+        next: (res) =>{
            this.detail = res;
            this.genres = res?.genres;
-              }
-           })
+              },
+        error: (res) => console.log(res)    
+      });
+      this.commentService.getComment(this.sessionServise.getUserId(),this.movieid).subscribe({
+        next: (res)=> this.comment = res.commentText,
+        error: (res)=>console.log(res) 
+      });
+      this.ratingService.getRating(this.sessionServise.getUserId(),this.movieid).subscribe({
+        next: (res)=> this.rating = this.getarray(res.Ratings.movie_rating),
+        error: (res)=> console.log(res)
+      });
+
 
   }
 
-
+  getarray(lenght:number){
+    return new Array(lenght);
+  }
 
 }
 
